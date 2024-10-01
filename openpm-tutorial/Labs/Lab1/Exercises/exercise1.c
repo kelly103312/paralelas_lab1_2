a95bcf4c6f87d87d3165070f478cbaac8fbb66e6
unknown <desarrollador1aj@gmail.com>
2024-10-01 18:50:31 -0500

    v2 ejercicio 1 lab 1

--- openpm-tutorial/Labs/Lab1/Exercises/exercise1.c
+++ openpm-tutorial/Labs/Lab1/Exercises/exercise1.c
@@ -4,7 +4,11 @@
 int main() {
     #pragma omp parallel num_threads(10)
     {
-        printf("Hello from thread %d\n", omp_get_thread_num());
+        if(omp_get_thread_num() %2 ==0){
+            printf("Hello from thread %d PAR\n", omp_get_thread_num());
+        }else{
+            printf("Hello from thread %d IMPAR\n", omp_get_thread_num());
+        }
     }
     return 0;
 }
