eca00a2df734651bf063865f06e93ea9170a82ba
unknown <desarrollador1aj@gmail.com>
2024-10-05 21:27:17 -0500

    Ejercicio 2 lab 1

--- openpm-tutorial/Labs/Lab1/Exercises/.ipynb_checkpoints/exercise2-checkpoint.c
+++ openpm-tutorial/Labs/Lab1/Exercises/.ipynb_checkpoints/exercise2-checkpoint.c
@@ -1,35 +1,38 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 
 int main() {
     int Array[100]; // Use rand() % 10000 for init
     int max = 0;
     int min = 100000; 
     int value = 0; 
-    int avg;
-
-     #pragma omp parallel num_threads(100)
-    {
-        //printf("Hello from thread %d \n", omp_get_thread_num());
+    int avg,sum;
+    srand(time(0));
+    for (int i = 0; i < 100; i++){
+        Array[i]=rand()%10000;
+    }
+    
+     #pragma omp parallel for reduction(max:max) reduction(min:min) reduction(+:sum) num_threads(100)
         for (int i = 0; i < 100; i++){
-           value= rand() % 10000;
-           printf("Valor dato  %d max %d min %d\n", Array[i], max,min);
-           Array[i] = value;
-           if(value > max){
-               max=  value;
-           }
-           if(value < min){
-               min = value;
-           }
-           avg += value;
+             max= Array[i];
+             min = Array[i];
+             sum += Array[i];
         }
-    } 
+    
+    avg= sum/100;
     printf("Valor minimo  %d \n", min);
     printf("Valor maximo  %d \n", max);
     printf("Valor avg  %d \n", avg);
+    
     for (int e = 0; e < 100; e++){
+        if(Array[e]> max){
+            printf("MAX %d\n",e);
+        }
+        if(Array[e]< min){
+            printf("MIN %d\n",e);
+        }
         printf("Elemento número %d  valor %d\n",e, Array[e]);
     }
     
